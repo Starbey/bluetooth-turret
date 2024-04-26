05302e1cb319ff3d659784fa8483983bf25d0eb8
Benjamin Gavieres <benjamin.gavieres@gmail.com>
2024-04-26 19:02:47 -0400

    only save best haar match

--- main/cv_bt.py
+++ main/cv_bt.py
@@ -11,8 +11,19 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.05, minNeighbors = 7, minSize = (100, 100)) # returns list of rectangles around faces
 
+    best_match = None
+    max_confidence = 10000 # max confidence = min area
+
     for (x, y, w, h) in faces:
+        confidence = w * h # confidence is proportional to area
+        if confidence > max_confidence:
+            best_match = (x, y, w, h)
+            max_confidence = confidence
+    
+    if best_match is not None:
+        (x, y, w, h) = best_match
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+        print("Face detected: %d %d" % (x, y) )
 
     cv2.imshow("frame", frame)
 
